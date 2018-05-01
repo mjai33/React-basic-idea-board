99512a25405792951647e4b30cef5624676388f7
Mohit Jain 10 <mjain30@sapient.com>
2018-05-01 13:46:26 +0530

    no need to inherit entire module , we need only render method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './containers/App.js';
 require('./styles/app.scss');
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
